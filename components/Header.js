c3813332a15917046f3f97987fd4a85ab81347d9
abxm2 <wulimaxh@gmail.com>
2022-12-14 14:33:48 -0700

    4

--- components/Header.js
+++ components/Header.js
@@ -70,7 +70,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                ✨ {BLOG.title}  
+                ✨ {BLOG.title}
               </div>
             </a>
 
